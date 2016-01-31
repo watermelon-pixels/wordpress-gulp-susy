1127a2c3190d837db842fa9d9179592245088ac0
[watermelon-pixels] <[contact@chantdeleau.com]>
2016-01-31 19:09:30 +0100

    Ajout Google fontes CDN en enqueue pour Roboto

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package watermelon-pixels
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'watermelon_pix' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'watermelon_pix' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'watermelon_pix' ), 'watermelon_pix', '<a href="http://watermelon-pixels.fr" rel="designer">Benoît</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
